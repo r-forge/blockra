1dce7ddc3b16114f20f4129ef968577c90bd20dc
kboudt (Kris Boudt) <kris.boudt@econ.kuleuven.be>
2015-06-15 19:30:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockra@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/blockra/R/ra.r
+++ pkg/blockra/R/ra.r
@@ -1,64 +1,42 @@
-#
-# Rearrangement Algorithm
-# 
+#' Rearrangement Algorithm
+#'
+#' Function which performs the traditional RA
+#'
+#' @param X numeric array or matrix
+#' @param epsilon target variance of row sums is epsilon multiplied by the mean of the matrix variances
+#' @param shuffle randomly permute each column of the matrix before rearrangement
+#'
+#' @return numeric matrix with a minimal row sum variance
+#'
+#' @export
+#'
+#' @seealso \code{\link{blockra}} for the block rearrangement algorithm
+#' @seealso \code{\link{brave}} for the block rearrangement variance equalizer
+#'
+#' @references \url{LINK TO RA PAPER}
+#'
+#' @author Kris Boudt, \email{kris.boudt@@vub.ac.be}
+#' @author Steven Vanduffel, \email{steven.vanduffel@@vub.ac.be}
+#' @author Kristof Verbeken, \email{kristof.verbeken@@vub.ac.be}
+ra <- function(X, epsilon = 0.1, shuffle = TRUE, fix.first = TRUE) {
+  if (shuffle) X <- shufflematrix(X, fix.first)
 
-ra <- function(X, epsilon = 0.1) {
-  # Apply Rearrangement Algorithm until variance of row sums
-  # is smaller than epsilon
-  #
-  # Args:
-  #   X: Original X to be rearranged
-  #   epsilon: Error margin
-  #
-  # Returns:
-  #   The rearranged X
-  
-  variance.new <- var(rowSums(X)) 
-  variance.old <- 2 * variance.new
-  iterations <- 0
-  time.start <- proc.time()
-  
-  target <- epsilon * mean(CalculateVariances(X))
-  
-  while (variance.new > target) {
+  var.new   <- var(rowSums(X))
+  var.old   <- 2 * var.new
+  target    <- epsilon * mean(apply(X, 2, var))
+
+  while ((var.new > target) && (var.new < var.old)) {
+
     for (col in 1 : ncol(X)) {
-      # Store current column
-      current <- X[, col]
-      
-      # Sort the current column
-      current <- sort(current)
-      
-      # Take sums of other columbs
-      other.sums <- rowSums(X[ , -col])
-      
-      # Rank other sums
+      current         <- sort(X[, col])
+      other.sums      <- rowSums(X[ , -col])
       other.sums.rank <- rank(-other.sums, ties.method = "random")
-      
-      # Save current col
-      X[, col] <- current[other.sums.rank]
+      X[, col]   <- current[other.sums.rank]
     }
-    
-    
-    
-    iterations <- iterations + 1
-    variance.old <- variance.new
-    variance.new <- var(rowSums(X))
-    
-    print(iterations)
-    print(variance.old)
-    print(variance.new)
-    
+
+    var.old <- var.new
+    var.new <- var(rowSums(X))
   }
-  
-  time.elapsed <- proc.time() - time.start
-  
-  cat("\r\n---------")
-  cat("\r\nRA")
-  cat("\r\n---------")
-  cat("\r\nIterations: ", iterations * ncol(X))
-  cat("\r\nTime: ", time.elapsed[3])
-  cat("\r\n---------")
-  
+
   return(X)
 }
-
